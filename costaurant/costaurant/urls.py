c447e63b4241ad7fa8b75522855ed90e55ca3d28
D.H Github <dablro1232@gmail.com>
2024-03-04 20:37:44 +0900

    first commit

---
+++ costaurant/costaurant/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for costaurant project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+"""
+urlpattenrs: url에 base 부분이 바귈때 어떻게 조치할지 볼 수 있음
+"""
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('foods/', include('foods.urls')), #foods앱에 foods앱안에 urls를 참조하도록 함.
+]
---
+++ costaurant/foods/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views 
+
+"""
+urlpattenrs: url에 base 부분이 바귈때 어떻게 조치할지 볼 수 있음
+"""
+urlpatterns = [
+    path('index/', views.index)
+]
